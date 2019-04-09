cb345e2ee81973196121bc6f4fa91c80edbdb420
NIndrokles <andreas.vedel@hotmail.com>
2019-04-09 10:06:36 +0200

    Changes to attack scripts

---
+++ Museum Guard/Assets/Flashlight.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+
+    public GameObject player;
+    float time = 1, timer = 0;
+    private Collider[] colls;
+    public float radius, coneSpreadAngles;
+    public int interactableLayer;
+    bool start = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnEnabled()
+    {
+        timer = 0;
+        time = 2;
+        player = GameObject.FindWithTag("Player");
+        transform.position = player.transform.position + player.transform.forward;
+        transform.LookAt(player.transform.position + player.transform.forward * 2);
+        start = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (start)
+        {
+            Debug.Log("Done with the shiiiit");
+            start = false;
+        }
+        //Time since initiation
+        timer += Time.deltaTime;
+
+        //Destruction timer
+        if (timer > time)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Museum Guard/Assets/Keyring.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyring : MonoBehaviour
+{
+    float time = 1, timer = 0;
+    public float radius;
+    
+    bool start = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (start)
+        {
+            Debug.Log("Done with the shiiiit");
+            start = false;
+        }
+        //Time since initiation
+        timer += Time.deltaTime;
+
+        //Destruction timer
+        if (timer > time)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
